9ad8ed6a72005f34fc178e876b0d53cb712e3cf3
Prakhathi <prakhathi@gmail.com>
2020-05-27 09:59:41 -0400

    Updates

---
+++ precache-manifest.01298640b0fb222b7046709a23f2dd3d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a69cea210f6a8a8a9d6a31eee5f65b",
+    "url": "/aboutme/index.html"
+  },
+  {
+    "revision": "c803e55e5a2615bfd15a",
+    "url": "/aboutme/static/css/main.a707d6d3.chunk.css"
+  },
+  {
+    "revision": "0635634d0252db72dd9a",
+    "url": "/aboutme/static/js/2.5b83bc46.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/aboutme/static/js/2.5b83bc46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c803e55e5a2615bfd15a",
+    "url": "/aboutme/static/js/main.ab31fe7a.chunk.js"
+  },
+  {
+    "revision": "866ddf1303470ad33925",
+    "url": "/aboutme/static/js/runtime-main.1acaead2.js"
+  },
+  {
+    "revision": "f1849f6db46c7e42f20ba54f021a7c88",
+    "url": "/aboutme/static/media/Bullet.f1849f6d.jpg"
+  },
+  {
+    "revision": "64ddfb6aaadc4c8d0f33de470eca296b",
+    "url": "/aboutme/static/media/Llama-2.64ddfb6a.jpg"
+  },
+  {
+    "revision": "825c91cffcb07f4b49bef2cc5bc818a6",
+    "url": "/aboutme/static/media/bg.825c91cf.jpg"
+  },
+  {
+    "revision": "f99a22311902b644962cb332ebdefa2f",
+    "url": "/aboutme/static/media/llam.f99a2231.jpg"
+  },
+  {
+    "revision": "8962c808582a0126962586773a034bc7",
+    "url": "/aboutme/static/media/llamas.8962c808.jpg"
+  },
+  {
+    "revision": "163970e66ccb460a29c84fe56263e29c",
+    "url": "/aboutme/static/media/toy.163970e6.jfif"
+  },
+  {
+    "revision": "11159d6638f14e09d8e97fffc3d2f37f",
+    "url": "/aboutme/static/media/wed.11159d66.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/profile/precache-manifest.7d67b7430d4d16439af5854ea915d0e2.js"
+  "/aboutme/precache-manifest.01298640b0fb222b7046709a23f2dd3d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/profile/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aboutme/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
